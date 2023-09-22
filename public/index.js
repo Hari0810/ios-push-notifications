4d5e0bd1c433a332f9f7ec4201514c7ce35ee0e9
Hari0810 <69460123+Hari0810@users.noreply.github.com>
2023-09-22 10:52:25 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,16 @@
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 async function run() {
   // A service worker must be registered in order to send notifications on iOS
   const registration = await navigator.serviceWorker.register(
@@ -8,40 +21,28 @@ async function run() {
   );
 
   const button = document.getElementById("subscribe");
+  button.addEventListener("click", async () => {
+    // Triggers popup to request access to send notifications
+    const result = await window.Notification.requestPermission();
 
-  const areNotificationsGranted = window.Notification.permission === "granted";
-  if (areNotificationsGranted) {
-    button.innerText = "Send Notification";
+    // If the user rejects the permission result will be "denied"
+    if (result === "granted") {
+      const subscription = await registration.pushManager.subscribe({
+        applicationServerKey: urlBase64ToUint8Array(
+          "[[INSERT YOUR PUBLIC VAPID KEY HERE]]"
+        ),
+        userVisibleOnly: true,
+      });
 
-    button.addEventListener("click", async () => {
-      await fetch("/send-notification");
-    });
-  } else {
-    button.addEventListener("click", async () => {
-      // Triggers popup to request access to send notifications
-      const result = await window.Notification.requestPermission();
-
-      // If the user rejects the permission result will be "denied"
-      if (result === "granted") {
-        const subscription = await registration.pushManager.subscribe({
-          // TODO: Replace with your public vapid key
-          applicationServerKey:
-            "BFkG2HKrQ3BYTS_4z1S1pRwNoX4vvQhCwi3q9Hum7nQ8p9FHU3nLAjzmGWet_63jkLD2XXFp2rgranujXvCJd4k",
-          userVisibleOnly: true,
-        });
-
-        await fetch("/save-subscription", {
-          method: "post",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(subscription),
-        });
-
-        window.location.reload();
-      }
-    });
-  }
+      await fetch("/save-subscription", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(subscription),
+      });
+    }
+  });
 }
 
 run();
