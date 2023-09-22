56b23e4d5115fdc97edcf5f64b4e5c8e0e16870b
Hari0810 <69460123+Hari0810@users.noreply.github.com>
2023-09-22 11:28:33 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,16 +1,3 @@
-function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
 async function run() {
   // A service worker must be registered in order to send notifications on iOS
   const registration = await navigator.serviceWorker.register(
@@ -27,19 +14,11 @@ async function run() {
 
     // If the user rejects the permission result will be "denied"
     if (result === "granted") {
-      const subscription = await registration.pushManager.subscribe({
-        applicationServerKey: urlBase64ToUint8Array(
-          "BGcWN7ZOyWsLjVH3bJfpKwDrOPJvRHMKOU81EV6-z41RmsF4UsNXnaoOjFqDjG16ZShBdQn2ckyXj-ZNIlauwzw"
-        ),
-        userVisibleOnly: true,
-      });
-
-      await fetch("/save-subscription", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(subscription),
+      // You must use the service worker notification to show the notification
+      // Using new Notification("Hello World", { body: "My first notification on iOS"}) does not work on iOS
+      // despite working on other platforms
+      await registration.showNotification("Hello World", {
+        body: "My first notification on iOS",
       });
     }
   });
